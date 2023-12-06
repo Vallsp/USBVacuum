96afa3de3246f8625ddedb4e52ccfa28b954c7c6
arpaflow <145016532+arpaflow@users.noreply.github.com>
2023-12-06 14:51:50 +0000

    Initial gui

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OUTPUT_DIR="./src/"
+
+# BEGIN: Check if tkdesigner is installed
+if ! [ -x "$(command -v tkdesigner)" ]; then
+  echo "Error: tkdesigner is not installed." >&2
+  exit 1
+fi
+
+# BEGIN: Check if .env file exists
+if [ ! -f .env ]; then
+  echo "Error: .env file not found."
+  exit 1
+fi
+# END: Check if .env file exists
+
+# BEGIN: Load environment variables from .env file
+source .env
+# END: Load environment variables from .env file
+
+# BEGIN: Build the application
+tkdesigner $FIGMA_URL $FIGMA_TOKEN -o $OUTPUT_DIR
+# END: Build the application
---
+++ install_dependencies.sh
@@ -0,0 +1,8 @@
+# Create a Python virtual environment
+python3 -m venv env
+
+# Activate the virtual environment
+source env/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
