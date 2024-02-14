e1415f96d791c4ce0e07ed848b8f75b801e57ef2
arpaflow <145016532+arpaflow@users.noreply.github.com>
2024-02-14 16:36:24 +0100

    Scan update

--- src/app/backend/scan.py
+++ src/app/backend/scan.py
@@ -3,6 +3,8 @@ import os
 from loguru import logger
 
 def do_scan(scan_type, usb_path):
+    if not os.path.exists(f"{os.environ['HOME']}/.clamav/quarantine"):
+        os.makedirs(f"{os.environ['HOME']}/.clamav/quarantine")
     if scan_type == 'complete':
         logger.info(f"Scanning {usb_path} with {scan_type} scan type.")
         complete_scan(usb_path)
@@ -14,7 +16,8 @@ def do_scan(scan_type, usb_path):
         messagebox.showerror("Erreur", "Please select a scan type.")
 
 def fast_scan(usb_path):
-    os.system(f'clamscan -r --max-filesize=100M {usb_path}')
+    #If clamav folder does not exist, create it
+    os.system(f'clamscan -r --move $HOME/.clamav/quarantine/ --max-filesize=100M {usb_path}')
 
 def complete_scan(usb_path):
-    os.system(f'clamscan -r {usb_path}')
+    os.system(f'clamscan -r --move $HOME/.clamav/quarantine/ {usb_path}')
