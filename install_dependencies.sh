14df3b3e6cf3d1d08f14e7be991e2468f82613e5
arpaflow <145016532+arpaflow@users.noreply.github.com>
2024-03-06 10:36:11 +0100

    fix 3

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -17,15 +17,3 @@ source venv/bin/activate
 
 # Install requirements
 pip install -r requirements.txt
-
-#Install packages
-sudo apt install python3 python3-tk python3-venv python3-pip clamav -y
-
-# Create a Python virtual environment
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
-# Install requirements
-pip install -r requirements.txt
