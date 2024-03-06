0bbb9a2e77a97c6543d7a242ba853bcb5b1dc66b
arpaflow <145016532+arpaflow@users.noreply.github.com>
2024-03-06 09:38:14 +0100

    fix install_dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,23 @@
+#/bin/bash
+
+# Check if script is run as root
+if [ "$EUID" -eq 0 ]
+    then echo "Please do not run as root"
+    exit
+fi
+
+#Install packages
+sudo apt install python3 python3-tk python3-venv python3-pip clamav -y
+
+# Create a Python virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
 #Install packages
 sudo apt install python3 python3-tk python3-venv python3-pip clamav -y
 
